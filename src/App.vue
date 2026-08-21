4a0925a9c03eef14137d9eb1d06ed64a54896fe6
JamieHa8794 <JamieHa8794@gmail.com>
2026-08-21 18:10:54 -0400

    style: implement desktop page canvas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import CryptoCalculator from '@/components/CryptoCalculator.vue'
+
+import '@/styles/App.css'
 </script>
 
 <template>
-  <main>
-    <CryptoCalculator />
+  <main class="page-canvas">
+    <div class="page-content">
+      <CryptoCalculator />
+    </div>
   </main>
 </template>
